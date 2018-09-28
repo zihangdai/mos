bf04f095f67819b5fc6979fd71abff598f76cc6a
peng xu <bly930725@gmail.com>
2018-09-28 13:24:50 -0400

    change finetune

--- finetune.py
+++ finetune.py
@@ -151,15 +151,16 @@ def evaluate(data_source, batch_size=10):
     total_loss = 0
     ntokens = len(corpus.dictionary)
     hidden = model.init_hidden(batch_size)
-    for i in range(0, data_source.size(0) - 1, args.bptt):
-        data, targets = get_batch(data_source, i, args, evaluation=True)
-        targets = targets.view(-1)
-        
-        log_prob, hidden = parallel_model(data, hidden)
-        loss = nn.functional.nll_loss(log_prob.view(-1, log_prob.size(2)), targets).data
+    with torch.no_grad():
+        for i in range(0, data_source.size(0) - 1, args.bptt):
+            data, targets = get_batch(data_source, i, args)
+            targets = targets.view(-1)
+            
+            log_prob, hidden = parallel_model(data, hidden)
+            loss = nn.functional.nll_loss(log_prob.view(-1, log_prob.size(2)), targets).data
 
-        total_loss += len(data) * loss
-        hidden = repackage_hidden(hidden)
+            total_loss += len(data) * loss
+            hidden = repackage_hidden(hidden)
     return total_loss[0] / len(data_source)
 
 def train():
@@ -210,7 +211,7 @@ def train():
             end = start + args.small_batch_size
 
         # `clip_grad_norm` helps prevent the exploding gradient problem in RNNs / LSTMs.
-        torch.nn.utils.clip_grad_norm(model.parameters(), args.clip)
+        torch.nn.utils.clip_grad_norm_(model.parameters(), args.clip)
         optimizer.step()
 
         # total_loss += raw_loss.data
