0caaf2c47aa8555d8ea5de12d258d1455ee3dfa7
peng xu <bly930725@gmail.com>
2018-10-19 16:45:40 -0400

    remove warnings

--- main.py
+++ main.py
@@ -168,7 +168,7 @@ def evaluate(data_source, batch_size=10):
             total_loss += loss * len(data)
 
             hidden = repackage_hidden(hidden)
-    return total_loss[0] / len(data_source)
+    return total_loss.item() / len(data_source)
 
 
 def train():
@@ -227,7 +227,7 @@ def train():
         # total_loss += raw_loss.data
         optimizer.param_groups[0]['lr'] = lr2
         if batch % args.log_interval == 0 and batch > 0:
-            cur_loss = total_loss[0] / args.log_interval
+            cur_loss = total_loss.item() / args.log_interval
             elapsed = time.time() - start_time
             logging('| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.2f} | ms/batch {:5.2f} | '
                     'loss {:5.2f} | ppl {:8.2f}'.format(
